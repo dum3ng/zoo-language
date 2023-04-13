074a2bd5d36537c4a8b858cb487b225f7d4c84bd
dumeng <dumeng81@outlook.com>
2023-04-13 19:03:48 +0800

    add validator

--- src/language-server/zoo-validator.ts
+++ src/language-server/zoo-validator.ts
@@ -1,31 +1,50 @@
-import { ValidationAcceptor, ValidationChecks } from 'langium';
-import { ZooAstType, Room  } from './generated/ast';
-import type { ZooServices } from './zoo-module';
+import { ValidationAcceptor, ValidationChecks } from 'langium'
+import { ZooAstType, Room } from './generated/ast'
+import type { ZooServices } from './zoo-module'
 
 /**
  * Register custom validation checks.
  */
 export function registerValidationChecks(services: ZooServices) {
-    const registry = services.validation.ValidationRegistry;
-    const validator = services.validation.ZooValidator;
+    const registry = services.validation.ValidationRegistry
+    const validator = services.validation.ZooValidator
     const checks: ValidationChecks<ZooAstType> = {
-        Room: validator.checkPersonStartsWithCapital
-    };
-    registry.register(checks, validator);
+        Room: [
+            validator.checkPersonStartsWithCapital,
+            validator.checkPrivateRoomOnlyOneResident,
+        ],
+    }
+    registry.register(checks, validator)
 }
 
 /**
  * Implementation of custom validations.
  */
 export class ZooValidator {
-
     checkPersonStartsWithCapital(room: Room, accept: ValidationAcceptor): void {
         if (room.name) {
-            const firstChar = room.name.substring(0, 1);
+            const firstChar = room.name.substring(0, 1)
             if (firstChar.toUpperCase() !== firstChar) {
-                accept('warning', 'Room name should start with a capital.', { node: room, property: 'name' });
+                accept('warning', 'Room name should start with a capital.', {
+                    node: room,
+                    property: 'name',
+                })
             }
         }
     }
 
+    checkPrivateRoomOnlyOneResident(
+        room: Room,
+        accept: ValidationAcceptor,
+    ): void {
+        if (room.private) {
+            if (room.residents.length > 1) {
+                accept(
+                    'error',
+                    'Private room can only live no more than one resident.',
+                    { node: room, property: 'private' },
+                )
+            }
+        }
+    }
 }
